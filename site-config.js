7eb96bb4649212b2f04713a54632ef86a530c468
Michael Broughton <mkbr20@users.noreply.github.com>
2026-07-22 16:55:14 +1000

    Connect private audience question form

--- site-config.js
+++ site-config.js
@@ -3,8 +3,8 @@
   Keep the quotation marks. Leave a value empty to show its placeholder state.
 */
 window.SITE_CONFIG = {
-  questionFormUrl: "",
-  questionQrImage: "",
+  questionFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSd9P72CuwdrmbYMGwgY3IvDiWemwGhaqiCQ23Gss8PVLIlRYg/viewform?usp=publish-editor",
+  questionQrImage: "assets/question-form-qr.svg",
   websiteTwoUrl: "",
   eventUrl: "https://office.ausit.org/event-6748203",
   productInfoChecked: "22 July 2026"
